9b18a5bda8ca5d7c345e8113caddf10dd631a0fa
Held Grijo <held.grijo@gmail.com>
2020-06-23 00:22:05 -0400

    Camera follow

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContoller : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 offset;
+
+    private Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + player.position.z);
+        transform.position = newPosition;
+    }
+}
