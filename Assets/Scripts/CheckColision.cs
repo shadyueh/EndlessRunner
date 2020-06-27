56c9c3a3a3efecd6be777bb7e52f6fab1433d698
Held Grijo <held.grijo@gmail.com>
2020-06-27 00:14:55 -0400

    Generating tiles dynamically

---
+++ Assets/Scripts/CheckColision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckColision : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered the Trigger");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Object is within the Trigger");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object has exited the Trigger");
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 31.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTile(0);
+        SpawnTile(2);
+        SpawnTile(3);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        zSpawn += tileLength;
+    }
+
+}
