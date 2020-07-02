a9a9b45ed915ac6684d6c5e27fd9fc5a79de710d
Held Grijo <held.grijo@gmail.com>
2020-07-02 13:37:56 -0400

    Retrying game

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Events : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RetryGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
